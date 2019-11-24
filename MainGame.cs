4c5a861760e5c215c856a5643a6ad7e1659091f9
Javier Fernández Barreiro <47840319+barrrettt@users.noreply.github.com>
2019-11-24 21:17:11 +0100

    Add screenshot
    Add screenshot and set main menu as main scene.

--- MainGame.cs
+++ MainGame.cs
@@ -18,8 +18,8 @@ public class MainGame : Node2D{
         OS.SetWindowMaximized(true);
 
         //load first scene directly
-        //PackedScene packedScene = (PackedScene) GD.Load("res://Scenes/MenuMain/MainMenu.tscn");//main menu
-        PackedScene packedScene = (PackedScene) GD.Load("res://Scenes/Game.tscn");//game
+        PackedScene packedScene = (PackedScene) GD.Load("res://Scenes/MenuMain/MainMenu.tscn");//main menu
+        //PackedScene packedScene = (PackedScene) GD.Load("res://Scenes/Game.tscn");//game
         AddChild(packedScene.Instance());
     }
     
